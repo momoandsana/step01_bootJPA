1456eaffbbbfe448cf1f640b48afd32701730e6e
hoseok <swiftie52@naver.com>
2024-10-31 14:28:14 +0900

    Initial commit

---
+++ src/main/java/web/mvc/Step01BootJpaApplication.java
@@ -0,0 +1,13 @@
+package web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step01BootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step01BootJpaApplication.class, args);
+	}
+
+}
